27129253cdde5229ef4cbc14922f06b426b88730
zhaohengsheng <hengsheng07@gmail.com>
2021-03-10 10:00:41 +0100

    init

---
+++ branches/ZHAO/Jeu de la vie/Assets/AllerMenuParametre.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AllerMenuParametre : MonoBehaviour
+{
+    public void AllerMenuParam()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ trunk/Jeu de la vie/Assets/script/RetourMenuSecondaire.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetourMenuSecondaire : MonoBehaviour
+{
+    public void RetourMenuSec()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
